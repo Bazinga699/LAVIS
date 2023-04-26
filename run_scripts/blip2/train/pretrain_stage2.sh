aaf9c9e6e2c9f5383c48d96da9ce81568f4253a0
1Bazinga699 <1402083730@qq.com>
2023-04-26 19:59:34 +0800

    add profiler

--- run_scripts/blip2/train/pretrain_stage2.sh
+++ run_scripts/blip2/train/pretrain_stage2.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+date
+tar -xf /home/bingxing2/home/scx6150/datasets/coco.tar -C /dev/shm
+tar -xf /home/bingxing2/home/scx6150/datasets/VG.tar -C /dev/shm
+date
 python -m torch.distributed.run --nproc_per_node=4 train.py --cfg-path /home/bingxing2/home/scx6150/code/LAVIS/lavis/projects/blip2/train/pretrain_stage2.yaml
